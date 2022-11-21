94563e2f04919c1f33801a5f84cc89ea2d11168c
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2022-11-20 22:47:20 -0300

    Create tabelas.js

---
+++ tabelas.js
@@ -0,0 +1,62 @@
+<table>
+   <caption>Produtos</caption>
+   <thead>
+        <tr>
+            <th>Produto</th>
+            <th>Preço</th>
+            <th>Quantidade</th>
+            <th>Total</th>
+        </tr>
+   </thead>
+   <tbody>
+        <tr>
+            <td>IPAD</td>
+            <td>3280,20</td>
+            <td>5</td>
+            <td>18000</td>
+        </tr>
+        <tr>
+            <td>Tablet</td>
+            <td>2000</td>
+            <td>1</td>
+            <td>2000</td>
+        </tr>
+   </tbody>
+   <tfoot>
+        <tr>
+            <td colspan="3">Total</td>
+            <td> 3778,2</td>
+
+        </tr>
+
+   </tfoot>
+
+</table>
+
+<style>
+    table{
+        border-collapse: collapse;
+
+    }
+    th , td{
+        padding: 10px;
+    }
+    thead tr {
+        border-bottom: solid 3px #444;
+    }
+    tbody tr:hover {
+        background-color: aquamarine;
+
+    }
+    tfoot {
+        background-color: #444;
+        color: white;
+        font-weight: bold;
+
+
+
+
+    }
+
+
+</style>
